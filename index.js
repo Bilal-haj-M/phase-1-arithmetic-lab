987c7ac88c802d97f84ef145c70fbb61db48190d
Bilal-hajM <bblal1003@gmail.com>
2023-04-07 02:38:17 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+const random = Math.random().toFixed(1) *10;
+
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(10, 20,);
